167a4f1cb160b79620efa6bb696c89aff0a9553a
Swerk <swerk.pro@gmail.com>
2022-06-17 23:29:16 +0200

    init

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <title>Portfolio</title>
+        <link rel="stylesheet" href="style.css"/>
+       
+    </head>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require "includes/header.php"; ?> 
+
+<html>
+    <body>
+        <div id="navbar">
+            <nav>
+                <div id="nom">
+                    <ul>
+                        <li><a href="#">OLIWER</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </body>
+</html>
