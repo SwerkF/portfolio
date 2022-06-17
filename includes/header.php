67c7b18e41fe65471d77a62994d213bc8132a3da
Swerk <swerk.pro@gmail.com>
2022-06-18 00:14:52 +0200

    init

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <title>Portfolio</title>
         <link rel="stylesheet" href="style.css"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=9" />
        
     </head>
 </html>
